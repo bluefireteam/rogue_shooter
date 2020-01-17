0d47ebb80823f4430ad9469f174657d77ab83c12
Erick Zanardo <erick.zanardo@dextra-sw.com>
2020-01-17 10:44:16 -0300

    web

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flame/flame.dart';
 import './game.dart';
 
 void main() async {
-  await Flame.util.setPortrait();
-  await Flame.util.fullScreen();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO Verify if is web
+  //await Flame.util.setPortrait();
+  //await Flame.util.fullScreen();
   Size size = await Flame.util.initialDimensions();
+
+  print(size.width);
 
   runApp(SpaceShooterGame(size).widget);
 }
