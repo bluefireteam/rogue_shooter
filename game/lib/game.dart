01b46004fe38b070e3ccc0bb9d5fde1cc845f37c
Erick Zanardo <erickzanardoo@gmail.com>
2020-09-18 20:26:02 -0300

    improving example

--- game/lib/game.dart
+++ game/lib/game.dart
@@ -4,6 +4,8 @@ import 'package:flame/time.dart';
 import 'package:flame/components/timer_component.dart';
 import 'package:flutter/material.dart';
 
+import 'dart:ui';
+
 import './components/player_component.dart';
 import './components/enemy_creator.dart';
 import './components/star_background_creator.dart';
@@ -15,7 +17,7 @@ class SpaceShooterGame extends BaseGame with PanDetector {
   PlayerComponent player;
   StarBackGroundCreator starBackGroundCreator;
 
-  bool _effect = false;
+  bool _effect = true;
 
   int score = 0;
   bool _musicStarted = false;
@@ -78,9 +80,13 @@ class SpaceShooterGame extends BaseGame with PanDetector {
     );
   }
 
-  final redFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFFFF0000).withOpacity(0.4), BlendMode.srcATop)..color = Color(0xFFFFFFFF).withOpacity(0.33);
-  final blueFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFF0000FF).withOpacity(0.4), BlendMode.srcATop)..color = Color(0xFFFFFFFF).withOpacity(0.33);
-  final greenFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFF00FF00).withOpacity(0.4), BlendMode.srcATop)..color = Color(0xFFFFFFFF).withOpacity(0.33);
+  // Variables for easy testing
+  static final blendMode = BlendMode.modulate;
+  static final opacity = 0.33;
+
+  final redFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFFFF0000).withOpacity(opacity), blendMode);
+  final blueFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFF0000FF).withOpacity(opacity), blendMode);
+  final greenFilter = Paint()..colorFilter = ColorFilter.mode(const Color(0xFF00FF00).withOpacity(opacity), blendMode);
 
   void toggleEffect() {
     _effect = !_effect;
@@ -89,16 +95,23 @@ class SpaceShooterGame extends BaseGame with PanDetector {
   @override
   void render(Canvas canvas) {
     if (_effect) {
-      canvas.saveLayer(Rect.largest, redFilter);
-      super.render(canvas);
-      canvas.restore();
-      canvas.translate(-2, 2);
+      final recorder = PictureRecorder();
+      final bufferCanvas = Canvas(recorder);
+      super.render(bufferCanvas);
+      final pic = recorder.endRecording();
+
+
+      canvas.drawPicture(pic);
       canvas.saveLayer(Rect.largest, blueFilter);
-      super.render(canvas);
+      canvas.drawPicture(pic);
       canvas.restore();
-      canvas.translate(-3, -1);
+      canvas.saveLayer(Rect.largest, redFilter);
+      canvas.translate(2, -1);
+      canvas.drawPicture(pic);
+      canvas.restore();
       canvas.saveLayer(Rect.largest, greenFilter);
-      super.render(canvas);
+      canvas.translate(-2, 1);
+      canvas.drawPicture(pic);
       canvas.restore();
     } else {
       super.render(canvas);
